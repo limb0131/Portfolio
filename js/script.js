794768432a031299b6d5ad484b012672f883603f
limb0131 <brianlim0201@gmail.com>
2025-01-07 18:08:01 -0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const navbarHeight = $(".navbar").outerHeight();
+const typeText = $(".text");
+
+$(document).ready(function () {
+  // jQuery code
+  Test();
+  typewriterText();
+  setInterval(typewriterText, 12000);
+
+  function Test() {
+    $(".navbar-brand").click(function () {
+      alert($(".navbar").outerHeight());
+    });
+  }
+
+  function typewriterText() {
+    setTimeout(() => {
+      $(".text").text("Frontend Developer");
+    }, 0);
+    setTimeout(() => {
+      $(".text").text("Game Developer");
+    }, 4000);
+    setTimeout(() => {
+      $(".text").text("UI/UX Designer");
+    }, 8000);
+  }
+});
